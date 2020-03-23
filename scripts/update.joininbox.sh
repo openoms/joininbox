e466e2c6fcf1c182eb2207fe2cfc3e4ba004a027
openoms <oms@tuta.io>
2020-03-23 09:16:56 +0000

    clean update script

--- scripts/update.joininbox.sh
+++ scripts/update.joininbox.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-sudo rm -rf /home/joinmarket/joininbox
-# sudo -u joinmarketgit clone https://github.com/openoms/joininbox.git
-sudo -u joinmarket mkdir /home/joinmarket/joininbox
-sudo -u joinmarket mkdir /home/joinin/joininbox/scripts/
-sudo cp ./scripts/* /home/joinin/joininbox/scripts/
-sudo chown -R joinmarket:joinmarket /home/joinmarket/joininbox/*
 
-sudo rm -f /home/joinin/*.sh
-sudo rm -f /home/joinin/*.py
+sudo rm -rf /home/joinmarket/joininbox
+sudo -u joinmarket git clone https://github.com/openoms/joininbox.git
+
+sudo rm -f /home/joinmarket/*.sh
+sudo rm -f /home/joinmarket/*.py
 
 sudo -u joinmarket cp /home/joinmarket/joininbox/scripts/* /home/joinmarket/
 sudo -u joinmarket chmod +x /home/joinmarket/*.sh
