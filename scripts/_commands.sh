275e84266d3d968959ff6fc1ed974715e0858ee9
openoms <43343391+openoms@users.noreply.github.com>
2020-05-08 21:59:30 +0100

    commands: add torthistx

--- scripts/_commands.sh
+++ scripts/_commands.sh
@@ -8,3 +8,13 @@ function menu() {
   cd /home/joinmarket
   ./menu.sh
 }
+
+# command: torthistx
+function torthistx() {
+  if [ $(cat /mnt/hdd/raspiblitz.conf 2>/dev/null | grep -c "runBehindTor=on") -eq 1 ]; then
+    echo "Broadcasting transaction through Tor to Blockstreams API and into the network."
+    curl --socks5-hostname localhost:9050 -d $1 -X POST http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/api/tx
+  else
+    echo "Not running behind Tor - to install run:"
+  fi
+}
