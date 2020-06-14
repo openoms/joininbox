99146dedb8668ece64c59d0cecbf09b481f6b2cf
openoms <oms@tuta.io>
2020-06-14 09:21:48 +0100

    formatting

--- scripts/_commands.sh
+++ scripts/_commands.sh
@@ -12,9 +12,9 @@ function menu() {
 # command: torthistx
 function torthistx() {
   if [ $(cat /mnt/hdd/raspiblitz.conf 2>/dev/null | grep -c "runBehindTor=on") -eq 1 ]; then
-    echo "Broadcasting transaction through Tor to Blockstreams API and into the network."
+    echo "Broadcasts a transaction through Tor to Blockstream's API and into the network."
     curl --socks5-hostname localhost:9050 -d $1 -X POST http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/api/tx
   else
-    echo "Not running behind Tor - to install run:"
+    echo "Not running behind Tor"
   fi
 }
