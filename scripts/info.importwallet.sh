b2b4f4c83f336d0af28562d2b622fccdad732d1e
openoms <oms@tuta.io>
2020-06-15 10:30:42 +0100

    importwallet: clarify suggested wallet paths
    fixing: https://github.com/openoms/joininbox/issues/1

--- scripts/info.importwallet.sh
+++ scripts/info.importwallet.sh
@@ -7,7 +7,7 @@ OPTIONS=(LAN "Computers on the same network" \
         TOR "Copy from a Tor enabled remote node"
         )
 
-CHOICE=$(dialog --clear --title "Would you like to copy over LAN or Tor?" --menu "" 11 60 6 "${OPTIONS[@]}" 2>&1 >/dev/tty)
+CHOICE=$(dialog --clear --title "Would you like to copy over LAN or Tor?" --menu "" 8 50 6 "${OPTIONS[@]}" 2>&1 >/dev/tty)
 
 clear
 case $CHOICE in
@@ -27,7 +27,7 @@ Do you want to install Joinmarket now?" 7 42
     errorOnInstall=$?
     if [ ${errorOnInstall} -eq 0 ]; then
       dialog --title "Installed JoinMarket" \
-        --msgbox "\nContinue from the menu or use the command line " 7 56
+        --msgbox "\nContinue from the menu or use the command line " 3 50
     else 
       DIALOGRC=.dialogrc.onerror dialog --title "Error during install" \
         --msgbox "\nPlease search or report at:\n https://github.com/openoms/joininbox/issues" 7 56
@@ -45,35 +45,48 @@ elif [ "${CHOICE}" = "TOR" ]; then
   TOR_ADDRESS=$(sudo cat $HiddenServiceDir/ssh/hostname)
 fi
 
-echo ""
-echo "************************************************************************************"
-echo "Instructions to COPY wallets from another computer"
-echo "************************************************************************************"
-echo ""
-echo "You can use the wallets from another JoinMarket instance"
-echo ""
+echo "
+************************************************************************************
+Instructions to COPY wallets from another computer
+************************************************************************************
+
+You can use the wallets from another JoinMarket instance
+"
 if [ "${CHOICE}" = "LAN" ]; then
-  echo "Both computers (your JoininBox and the other computer with the wallets) need"
-  echo "to be connected to the same local network."
+  if [ -f "/mnt/hdd/raspiblitz.conf" ] ; then
+    echo "Both computers (the RaspiBlitz and the source computer with the wallet(s))" 
+  else
+    echo "Both computers (the JoininBox and the source computer with the wallet(s))"
+  fi
+echo "need to be connected to the same local network."
 elif [ "${CHOICE}" = "TOR" ]; then
   echo "The remote node needs to have Tor activated to be able to use torify"
 fi 
-echo ""
-echo "Open a terminal on the source computer and change into the directory that contains the"
-echo "wallets. Usually: ~/.joinmarket/wallets or ~/joinmarket-clientserver/scripts/wallets"
-echo "You should see files called '.jmdat'"
-echo ""
-echo "COPY, PASTE & EXECUTE the following command in the terminal of the source computer:"
-echo ""
+echo "
+Open a terminal connected to the source computer and change into
+the directory containing the wallets. 
+
+By default the path is: '~/.joinmarket/wallets' or 
+in older versions: '~/joinmarket-clientserver/scripts/wallets'
+
+You should see files called '.jmdat'
+
+COPY, PASTE & EXECUTE the following command in the terminal of the source computer:
+"
 if [ "${CHOICE}" = "LAN" ]; then
   echo "scp ./*.jmdat joinmarket@${localip}:~/.joinmarket/wallets"
 elif [ "${CHOICE}" = "TOR" ]; then
   echo "torify scp ./*.jmdat joinmarket@${TOR_ADDRESS}:~/.joinmarket/wallets"
 fi
 echo "" 
-echo "This command will ask for your SSH PASSWORD of your JoininBox."
-echo "Use the PASSWORD_B on a RaspiBlitz (same as the rpcpassword in the bitcoin.conf)."
-echo "************************************************************************************"
-echo "PRESS ENTER if the transfer is done OR if you want to choose another option."
+if [ -f "/mnt/hdd/raspiblitz.conf" ] ; then
+  echo "Use the PASSWORD_B to authorize the file transfer
+(same as the rpcpassword in the /mnt/hdd/bitcoin/bitcoin.conf)."
+else
+  echo "This command will ask for the SSH PASSWORD of the JoininBox."
+fi
+echo "************************************************************************************
+PRESS ENTER if the transfer is done OR if you want to choose another option.
+"
 sleep 2
 read key
