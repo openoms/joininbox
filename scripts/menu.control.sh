812d7d7b81e49873a1cee0753604dbfbba5ec9ca
openoms <oms@tuta.io>
2020-06-17 11:06:34 +0100

    add menu.control.sh

---
+++ scripts/menu.control.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# unlocking through stdin does not work with the freeze method:
+# https://github.com/JoinMarket-Org/joinmarket-clientserver/issues/598
+# /home/joinmarket/start.script.sh wallet-tool $(cat $wallet) freeze $(cat $mixdepth)
+
+source menu.functions.sh
+
+# get wallet
+wallet=$(tempfile 2>/dev/null)
+dialog --backtitle "Choose a wallet" \
+--title "Choose a wallet by typing the full name of the file" \
+--fselect "/home/joinmarket/.joinmarket/wallets/" 10 60 2> $wallet
+openMenuIfCancelled $?
+
+# get mixdepth
+mixdepth=$(tempfile 2>/dev/null)
+dialog --backtitle "Choose a mixdepth" \
+--inputbox "Type a number between 0 to 4 to choose the mixdepth" 8 60 2> $mixdepth
+openMenuIfCancelled $?
+
+echo "Run the following command manually to use the freeze method:
+
+python ~/joinmarket-clientserver/scripts/wallet-tool.py -m$(cat $mixdepth) $(cat $wallet) freeze
+
+type 'menu' and press ENTER to return to the menu
+"
