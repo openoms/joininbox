29e0cc0be10efef21655448f30987435c1a2983c
openoms <oms@tuta.io>
2020-06-18 14:51:11 +0100

    update: show update steps (radical method)

--- scripts/update.joininbox.sh
+++ scripts/update.joininbox.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo "Removing the joininbox directory.."
+echo ""
 sudo rm -rf /home/joinmarket/joininbox
+echo ""
+echo "Cloning the latest state from https://github.com/openoms/joininbox"
+echo ""
 sudo -u joinmarket git clone https://github.com/openoms/joininbox.git
-
-sudo -u joinmarket cp ./joininbox/scripts/* /home/joinmarket/
-sudo -u joinmarket cp ./joininbox/scripts/.* /home/joinmarket/ 2>/dev/null
+echo ""
+echo "Copy the scripts in place"
+echo ""
+sudo -u joinmarket cp /home/joinmarket/joininbox/scripts/* /home/joinmarket/
+sudo -u joinmarket cp /home/joinmarket/joininbox/scripts/.* /home/joinmarket/ 2>/dev/null
 sudo -u joinmarket chmod +x /home/joinmarket/*.sh
