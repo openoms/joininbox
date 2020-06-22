43210eac25e9ad250d83e6338e09492755b91e3c
openoms <oms@tuta.io>
2020-06-22 15:13:57 +0100

    add chooseWallet function

--- scripts/menu.functions.sh
+++ scripts/menu.functions.sh
@@ -1,3 +1,4 @@
+# openMenuIfCancelled
 openMenuIfCancelled() {
 pressed=$1
 case $pressed in
@@ -14,4 +15,13 @@ case $pressed in
     /home/joinmarket/menu.sh
     exit 1;;
 esac
+}
+
+# chooseWallet
+chooseWallet() {
+wallet=$(tempfile 2>/dev/null)
+dialog --backtitle "Choose a wallet" \
+--title "Choose a wallet by typing the full name of the file" \
+--fselect "/home/joinmarket/.joinmarket/wallets/" 10 60 2> $wallet
+openMenuIfCancelled $?
 }
