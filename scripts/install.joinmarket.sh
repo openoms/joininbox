b396d0d4d81fcdb793eccb72ee67198168a44d06
openoms <43343391+openoms@users.noreply.github.com>
2020-08-25 17:05:03 +0100

    install.joinmarket: improve output and comments

--- scripts/install.joinmarket.sh
+++ scripts/install.joinmarket.sh
@@ -2,15 +2,16 @@
 
 # functions
 
-function install_joinmarket() {
+function installJoinMarket() {
+  version="v0.7.0"
   cd /home/joinmarket
   # PySide2 for armf: https://packages.debian.org/buster/python3-pyside2.qtcore
-  echo "# installing ARM specific dependencies to run the QT GUI on ARM"
+  echo "# installing ARM specific dependencies for the QT GUI"
   sudo apt install -y python3-pyside2.qtcore python3-pyside2.qtgui python3-pyside2.qtwidgets zlib1g-dev libjpeg-dev
   echo "# installing JoinMarket"
   sudo -u joinmarket git clone https://github.com/Joinmarket-Org/joinmarket-clientserver
   cd joinmarket-clientserver
-  git reset --hard v0.7.0
+  git reset --hard $version
   # make install.sh set up jmvenv with -- system-site-packages
   sed -i "s#^    virtualenv -p \"\${python}\" \"\${jm_source}/jmvenv\" || return 1#\
   virtualenv --system-site-packages -p \"\${python}\" \"\${jm_source}/jmvenv\" || return 1#g" \
@@ -23,6 +24,7 @@ function install_joinmarket() {
   /home/joinmarket/joinmarket-clientserver/jmvenv/bin/python -c 'import PySide2'
   pip install qrcode[pil]
   pip install https://github.com/sunu/qt5reactor/archive/58410aaead2185e9917ae9cac9c50fe7b70e4a60.zip#egg=qt5reactor
+  echo "# installed JoinMarket $version"
 }
 
 source /home/joinmarket/joinin.conf
@@ -30,9 +32,9 @@ source /home/joinmarket/joinin.conf
 if [ "$1" = "install" ]; then
   # install joinmarket
   if [ ! -f "/home/joinmarket/joinmarket-clientserver/jmvenv/bin/activate" ] ; then
-    install_joinmarket
+    installJoinMarket
   else
-    echo "JoinMarket is already installed"
+    echo "# JoinMarket is already installed"
     echo ""
   fi
   exit 0
@@ -41,13 +43,13 @@ fi
 if [ "$1" = "config" ]; then
   # generate joinmarket.cfg
   if [ ! -f "/home/joinmarket/.joinmarket/joinmarket.cfg" ] ; then
-    echo "Generating the joinmarket.cfg"
+    echo "# generating the joinmarket.cfg"
     echo ""
     . /home/joinmarket/joinmarket-clientserver/jmvenv/bin/activate &&\
     cd /home/joinmarket/joinmarket-clientserver/scripts/
     python wallet-tool.py generate --datadir=/home/joinmarket/.joinmarket
   else
-    echo "the joinmarket.cfg is already present"
+    echo "# the joinmarket.cfg is already present"
     echo ""
   fi
   # set strict permission to joinmarket.cfg
@@ -79,8 +81,10 @@ if [ "$1" = "config" ]; then
 fi
 
 if [ "$1" = "update" ]; then
+  echo "# exiting jmvenv"
   deactivate
+  echo "# deleting the joinmarket-clientserver directory"
   sudo rm -rf /home/joinmarket/joinmarket-clientserver
-  install_joinmarket
+  installJoinMarket
   exit 0
 fi
