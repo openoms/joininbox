255433d5d9e68612b830cace3f5f4ecb4e80eb19
openoms <oms@tuta.io>
2020-08-25 12:42:31 +0100

    install.joinmarket: update to v0.7.0

--- scripts/install.joinmarket.sh
+++ scripts/install.joinmarket.sh
@@ -4,25 +4,27 @@ source /home/joinmarket/joinin.conf
 
 # install joinmarket
 if [ ! -f "/home/joinmarket/joinmarket-clientserver/jmvenv/bin/activate" ] ; then
-  # install joinmarket
   cd /home/joinmarket
   # PySide2 for armf: https://packages.debian.org/buster/python3-pyside2.qtcore
+  echo "# installing ARM specific dependencies to run the QT GUI on ARM"
   sudo apt install -y python3-pyside2.qtcore python3-pyside2.qtgui python3-pyside2.qtwidgets zlib1g-dev libjpeg-dev
-  # from https://github.com/JoinMarket-Org/joinmarket-clientserver/blob/master/docs/INSTALL.md 
-  sudo apt install -y python3-dev python3-pip git build-essential automake pkg-config libtool libffi-dev libssl-dev libgmp-dev libsodium-dev
+  echo "# installing JoinMarket"
   sudo -u joinmarket git clone https://github.com/Joinmarket-Org/joinmarket-clientserver
   cd joinmarket-clientserver
-  git reset --hard v0.6.3.1
-  # set up jmvenv 
-  sudo apt install -y virtualenv
-  # use the PySide2 armf package from the system
-  sudo -u joinmarket virtualenv --system-site-packages -p /usr/bin/python3.7 jmvenv
+  git reset --hard v0.7.0
+  # make install.sh set up jmvenv with -- system-site-packages
+  sed -i "s#^    virtualenv -p \"\${python}\" \"\${jm_source}/jmvenv\" || return 1#\
+  virtualenv --system-site-packages -p \"\${python}\" \"\${jm_source}/jmvenv\" || return 1#g" \
+  install.sh
+  ./install.sh --with-qt
+  
+  echo "# installing python requirements to run the QT GUI on ARM"    
   source jmvenv/bin/activate || exit 1
-  pip install -r requirements/base.txt
-  # https://github.com/JoinMarket-Org/joinmarket-clientserver/blob/master/requirements/gui.txt
+  # use the PySide2 armf package from the system
   /home/joinmarket/joinmarket-clientserver/jmvenv/bin/python -c 'import PySide2'
   pip install qrcode[pil]
   pip install https://github.com/sunu/qt5reactor/archive/58410aaead2185e9917ae9cac9c50fe7b70e4a60.zip#egg=qt5reactor
+
 else
   echo "JoinMarket is already installed"
   echo ""
@@ -40,8 +42,7 @@ else
   echo "the joinmarket.cfg is already present"
   echo ""
 fi
-
-# make sure permission 600
+# set strict permission
 sudo chmod 600 /home/joinmarket/.joinmarket/joinmarket.cfg || exit 1
 
 if [ ${runBehindTor} = "on" ]; then
