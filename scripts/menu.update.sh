195b4a61db66999520cad4e5cdf7f122df6e8684
openoms <oms@tuta.io>
2020-10-17 21:52:32 +0100

    update: show current versions in dialog

--- scripts/menu.update.sh
+++ scripts/menu.update.sh
@@ -2,19 +2,24 @@
 
 source /home/joinmarket/_functions.sh
 
+currentJBversion=$(cd $HOME/joininbox; git describe --tags)
+currentJMversion=$(cd $HOME/joinmarket-clientserver; git describe --tags)
+
 # BASIC MENU INFO
-HEIGHT=8
-WIDTH=55
+HEIGHT=11
+WIDTH=56
 CHOICE_HEIGHT=2
 TITLE="Update options"
-MENU=""
+MENU="
+Current JoininBox version: $currentJBversion
+Current JoinMarket version: $currentJMversion"
 OPTIONS=()
 BACKTITLE="JoininBox GUI"
 
 # Basic Options
 OPTIONS+=(\
   JOININBOX "Update the JoininBox scripts and menu" \
-  JOINMARKET "Update JoinMarket to $(grep JMVersion= < ~/_functions.sh | cut -d '"' -f 2)" \
+  JOINMARKET "Update/reinstall JoinMarket to $(grep JMVersion= < ~/_functions.sh | cut -d '"' -f 2)" \
 )
 
 CHOICE=$(dialog --clear \
