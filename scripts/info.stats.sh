04f9b02b34e87bd57b161628a0f28737c40ef1fb
openoms <oms@tuta.io>
2021-01-30 09:58:18 +0000

    stats: remove column 'all' to not overflow on LCD

--- scripts/info.stats.sh
+++ scripts/info.stats.sh
@@ -5,7 +5,7 @@ source /home/joinmarket/_functions.sh
 feereport
 YGuptime
 
-echo "JoinMarket stats:day:week:month:all
-coinjoins as a Maker:$dayCoinjoins:$weekCoinjoins:$monthCoinjoins:$allCoinjoins
-sats earned:$dayEarned:$weekEarned:$monthEarned:$allEarned
+echo "JoinMarket stats:day:week:month
+coinjoins as a Maker:$dayCoinjoins:$weekCoinjoins:$monthCoinjoins
+sats earned:$dayEarned:$weekEarned:$monthEarned
 Maker uptime:$JMUptime" | column -t -s:
