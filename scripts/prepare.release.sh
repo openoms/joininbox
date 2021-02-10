29420fe26ddc9bf5b6e19a0dd1fc074ada7782b2
openoms <oms@tuta.io>
2021-02-10 11:58:57 +0000

    fix file permissions in git

---
+++ scripts/prepare.release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# based on https://github.com/rootzoll/raspiblitz/blob/v1.6/home.admin/XXprepareRelease.sh
+
+# Run this script once after a fresh sd card build
+# to prepare the image for release as a downloadable sd card image
+
+# SSH Pubkeys (make unique for every sd card image install)
+echo "deleting SSH Pub keys ..."
+echo "they will get recreated on fresh bootup, by _bootstrap.sh service"
+sudo rm /etc/ssh/ssh_host_*
+echo "OK"
+
+# https://github.com/rootzoll/raspiblitz/issues/1068#issuecomment-599267503
+echo
+echo "deleting local DNS confs ..."
+sudo rm /etc/resolv.conf
+echo "OK"
+
+# https://github.com/rootzoll/raspiblitz/issues/1371
+echo
+echo "deleting local WIFI conf ..."
+sudo rm /boot/wpa_supplicant.conf 2>/dev/null
+echo "OK"
+
+echo 
+echo "Will shutdown now."
+echo "Wait until the SBC LEDs show no activity anymore."
+echo "Then remove SD card and make a release image from it."
+sudo shutdown now
