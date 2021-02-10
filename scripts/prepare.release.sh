25a899126860b9561faffc2807d16d90bb78d167
openoms <oms@tuta.io>
2021-02-10 18:50:23 +0000

    add prepare.release.sh

--- scripts/prepare.release.sh
+++ scripts/prepare.release.sh
@@ -4,26 +4,31 @@
 # Run this script once after a fresh sd card build
 # to prepare the image for release as a downloadable sd card image
 
+# SSH authorized keys
+echo "# Deleting SSH authorized_keys ..."
+sudo rm /root/.ssh/authorized_keys
+echo "# OK"
+
 # SSH Pubkeys (make unique for every sd card image install)
-echo "deleting SSH Pub keys ..."
-echo "they will get recreated on fresh bootup, by _bootstrap.sh service"
+echo "# Deleting SSH Pub keys ..."
+echo "they will get recreated on fresh bootup, by the bootstrap.service"
 sudo rm /etc/ssh/ssh_host_*
-echo "OK"
+echo "# OK"
 
 # https://github.com/rootzoll/raspiblitz/issues/1068#issuecomment-599267503
 echo
-echo "deleting local DNS confs ..."
+echo "# Deleting local DNS confs ..."
 sudo rm /etc/resolv.conf
-echo "OK"
+echo "# OK"
 
 # https://github.com/rootzoll/raspiblitz/issues/1371
 echo
-echo "deleting local WIFI conf ..."
+echo "# Deleting local WIFI conf ..."
 sudo rm /boot/wpa_supplicant.conf 2>/dev/null
-echo "OK"
+echo "# OK"
 
 echo 
-echo "Will shutdown now."
-echo "Wait until the SBC LEDs show no activity anymore."
-echo "Then remove SD card and make a release image from it."
+echo "# Will shutdown now."
+echo "# Wait until the SBC LEDs show no activity anymore."
+echo "# Then remove SD card and make a release image from it."
 sudo shutdown now
