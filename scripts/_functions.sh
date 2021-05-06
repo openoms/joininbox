1b3dc44709e06816ec2c204eab60acfd16e0f407
openoms <oms@tuta.io>
2021-05-06 23:25:12 +0100

    avoid menu loops on Cancel and ESC
    fixes: https://github.com/openoms/joininbox/issues/49

--- scripts/_functions.sh
+++ scripts/_functions.sh
@@ -30,16 +30,14 @@ function openMenuIfCancelled() {
   pressed=$1
   case $pressed in
     1)
+      clear
       echo "# Cancelled"
-      echo "# Returning to the menu..."
       sleep 1
-      /home/joinmarket/menu.sh
       exit 1;;
     255)
-      echo "# ESC pressed."
-      echo "# Returning to the menu..."
+      clear
+      echo "# ESC pressed"
       sleep 1
-      /home/joinmarket/menu.sh
       exit 1;;
   esac
 }
