ae3da9840d256b623c794624765284dec6ae997b
openoms <oms@tuta.io>
2022-07-30 15:34:27 +0100

    convert to new format

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -6,7 +6,7 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update -y && sudo apt-get install packer -y
 
-echo -e "Installing GO..."
+echo -e "Installing Go..."
 wget --progress=bar:force https://go.dev/dl/go1.18.4.linux-arm64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.18.4.linux-arm64.tar.gz
 sudo rm -rf go1.18.4.linux-arm64.tar.gz
@@ -21,10 +21,10 @@ go build
 
 # Move json file to packer folder
 echo -e "\nMoving json file and scripts to the packer folder..."
-wget --progress=bar:force https://raw.githubusercontent.com/openoms/joininbox/packer/.github/scripts/joininbox-arm64.json
-wget --progress=bar:force https://raw.githubusercontent.com/openoms/joininbox/packer/.github/scripts/packages.config
-wget --progress=bar:force https://raw.githubusercontent.com/openoms/joininbox/packer/build_joininbox.sh
+cp ../joininbox-arm64.pkr.hcl ./
+cp ../packages.config ./
+wget --progress=bar:force -O build_joininbox.sh https://raw.githubusercontent.com/openoms/joininbox/packer/build_joininbox.sh
 
-# Build packer
+# Build the image in docker
 echo -e "\nBuilding packer image..."
-docker run --rm --privileged -v /dev:/dev -v ${PWD}:/build mkaczanowski/packer-builder-arm build joininbox-arm64.json
+docker run --rm --privileged -v /dev:/dev -v ${PWD}:/build mkaczanowski/packer-builder-arm build joininbox-arm64.pkr.hcl
