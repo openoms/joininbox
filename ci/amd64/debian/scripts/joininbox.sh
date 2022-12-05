d6f494f75140394e5604935c127fac6ac4c1e8cc
openoms <oms@tuta.io>
2022-12-05 20:03:16 +0000

    download the build script from PR branch

--- ci/amd64/debian/scripts/joininbox.sh
+++ ci/amd64/debian/scripts/joininbox.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
 
 echo 'Download the build_joininbox.sh script ...'
-wget https://raw.githubusercontent.com/openoms/joininbox/master/build_joininbox.sh
+wget https://raw.githubusercontent.com/${github_user}/joininbox/${branch}/build_joininbox.sh
 
 echo 'Build Joininbox ...'
 sudo bash build_joininbox.sh "${github_user}" "${branch}"
