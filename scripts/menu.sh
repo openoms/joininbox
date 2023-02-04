f4e1dac49552e8c43fc5d99c836c91251b4209e0
openoms <43343391+openoms@users.noreply.github.com>
2023-02-04 19:44:00 +0000

    Add Jam menu instead of QTGUI for the Raspiblitz on bullseye (#115)
    * fix shutdown and reboot for raspiblitz
    * add the Jam menu instead of qtgui for raspiblitz
    * don't install the qtgui on raspiblitz bullseye

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -15,134 +15,156 @@ Choose from the options:"
 OPTIONS=()
 
 # Basic Options
-OPTIONS+=(START   "Quickstart with JoinMarket")
-if [ "${qtgui}" = "true"  ]; then
-  OPTIONS+=(QTGUI   "Show how to open the JoinMarketQT GUI")
-  HEIGHT=$((HEIGHT+1))
-  CHOICE_HEIGHT=$((CHOICE_HEIGHT+1))
+OPTIONS+=(START "Quickstart with JoinMarket")
+if [ "${runningEnv}" = raspiblitz ] && [ $(lsb_release -sc) = bullseye ]; then
+  OPTIONS+=(JAM "JoinMarket Web UI options")
+  HEIGHT=$((HEIGHT + 1))
+  CHOICE_HEIGHT=$((CHOICE_HEIGHT + 1))
+elif [ "${qtgui}" = "true" ]; then
+  OPTIONS+=(QTGUI "Show how to open the JoinMarketQT GUI")
+  HEIGHT=$((HEIGHT + 1))
+  CHOICE_HEIGHT=$((CHOICE_HEIGHT + 1))
 fi
 OPTIONS+=("" ""
-  WALLET  "Wallet management options"
-  MAKER   "Yield Generator options"
+  WALLET "Wallet management options"
+  MAKER "Yield Generator options"
   "" ""
-  SEND    "Pay to an address with/without a coinjoin"
-  FREEZE  "Exercise coin control within a mixdepth"
+  SEND "Pay to an address with/without a coinjoin"
+  FREEZE "Exercise coin control within a mixdepth"
   PAYJOIN "Send/Receive between JoinMarket wallets"
   "" ""
-  OFFERS  "Watch the Order Book locally"
+  OFFERS "Watch the Order Book locally"
   "" ""
-  CONFIG "Connection and joinmarket.cfg settings" \
-  TOOLS  "Extra helper functions and services")
-  if [ "${runningEnv}" != mynode ]; then
-    OPTIONS+=(UPDATE "Update JoininBox or JoinMarket")
-    OPTIONS+=("" "")
-    if [ "${runningEnv}" = raspiblitz ]; then
-      OPTIONS+=(BLITZ "Switch to the RaspiBlitz menu")
-      HEIGHT=$((HEIGHT+1))
-      CHOICE_HEIGHT=$((CHOICE_HEIGHT+1))
-    fi
-    OPTIONS+=(REBOOT "Restart the computer")
-    OPTIONS+=(SHUTDOWN "Switch off the computer")
-    HEIGHT=$((HEIGHT+4))
-    CHOICE_HEIGHT=$((CHOICE_HEIGHT+4))
+  CONFIG "Connection and joinmarket.cfg settings"
+  TOOLS "Extra helper functions and services")
+if [ "${runningEnv}" != mynode ]; then
+  OPTIONS+=(UPDATE "Update JoininBox or JoinMarket")
+  OPTIONS+=("" "")
+  if [ "${runningEnv}" = raspiblitz ]; then
+    OPTIONS+=(BLITZ "Switch to the RaspiBlitz menu")
+    HEIGHT=$((HEIGHT + 1))
+    CHOICE_HEIGHT=$((CHOICE_HEIGHT + 1))
   fi
+  OPTIONS+=(REBOOT "Restart the computer")
+  OPTIONS+=(SHUTDOWN "Switch off the computer")
+  HEIGHT=$((HEIGHT + 4))
+  CHOICE_HEIGHT=$((CHOICE_HEIGHT + 4))
+fi
 
 CHOICE=$(dialog \
-          --clear \
-          --backtitle "$BACKTITLE" \
-          --title "$TITLE" \
-          --ok-label "Select" \
-          --cancel-label "Exit" \
-          --menu "$MENU" \
-            $HEIGHT $WIDTH $CHOICE_HEIGHT \
-            "${OPTIONS[@]}" \
-            2>&1 >/dev/tty)
+  --clear \
+  --backtitle "$BACKTITLE" \
+  --title "$TITLE" \
+  --ok-label "Select" \
+  --cancel-label "Exit" \
+  --menu "$MENU" \
+  $HEIGHT $WIDTH $CHOICE_HEIGHT \
+  "${OPTIONS[@]}" \
+  2>&1 >/dev/tty)
 
 case $CHOICE in
-  START)
-    /home/joinmarket/menu.quickstart.sh
-    waitKeyOnExit1 $?
-    /home/joinmarket/menu.sh;;
-  WALLET)
-    /home/joinmarket/menu.wallet.sh
-    waitKeyOnExit1 $?
-    /home/joinmarket/menu.sh;;
-  QTGUI)
-    /home/joinmarket/info.qtgui.sh
-    /home/joinmarket/menu.sh;;
-  MAKER)
-    /home/joinmarket/menu.yg.sh
-    waitKeyOnExit1 $?
-    /home/joinmarket/menu.sh;;
-  SEND)
-    /home/joinmarket/menu.send.sh
-    echo ""
-    echo "Press ENTER to return to the menu..."
-    read key
-    /home/joinmarket/menu.sh;;
-  FREEZE)
-    /home/joinmarket/menu.freeze.sh
-    echo ""
-    echo "Press ENTER to return to the menu..."
-    read key
-    /home/joinmarket/menu.sh;;
-  PAYJOIN)
-    /home/joinmarket/menu.payjoin.sh
-    waitKeyOnExit1 $?
-    /home/joinmarket/menu.sh;;
-  OFFERS)
-    /home/joinmarket/menu.orderbook.sh
-    /home/joinmarket/menu.sh;;
-  CONFIG)
-    /home/joinmarket/menu.config.sh
-    echo "Returning to the menu..."
-    sleep 1
-    /home/joinmarket/menu.sh;;
-  TOOLS)
-    /home/joinmarket/menu.tools.sh
-    /home/joinmarket/menu.sh;;
-  UPDATE)
-    /home/joinmarket/menu.update.sh
-    /home/joinmarket/menu.sh;;
-  REBOOT)
+START)
+  /home/joinmarket/menu.quickstart.sh
+  waitKeyOnExit1 $?
+  /home/joinmarket/menu.sh
+  ;;
+WALLET)
+  /home/joinmarket/menu.wallet.sh
+  waitKeyOnExit1 $?
+  /home/joinmarket/menu.sh
+  ;;
+QTGUI)
+  /home/joinmarket/info.qtgui.sh
+  /home/joinmarket/menu.sh
+  ;;
+JAM)
+  /home/joinmarket/menu.jam.sh
+  /home/joinmarket/menu.sh
+  ;;
+MAKER)
+  /home/joinmarket/menu.yg.sh
+  waitKeyOnExit1 $?
+  /home/joinmarket/menu.sh
+  ;;
+SEND)
+  /home/joinmarket/menu.send.sh
+  echo ""
+  echo "Press ENTER to return to the menu..."
+  read key
+  /home/joinmarket/menu.sh
+  ;;
+FREEZE)
+  /home/joinmarket/menu.freeze.sh
+  echo ""
+  echo "Press ENTER to return to the menu..."
+  read key
+  /home/joinmarket/menu.sh
+  ;;
+PAYJOIN)
+  /home/joinmarket/menu.payjoin.sh
+  waitKeyOnExit1 $?
+  /home/joinmarket/menu.sh
+  ;;
+OFFERS)
+  /home/joinmarket/menu.orderbook.sh
+  /home/joinmarket/menu.sh
+  ;;
+CONFIG)
+  /home/joinmarket/menu.config.sh
+  echo "Returning to the menu..."
+  sleep 1
+  /home/joinmarket/menu.sh
+  ;;
+TOOLS)
+  /home/joinmarket/menu.tools.sh
+  /home/joinmarket/menu.sh
+  ;;
+UPDATE)
+  /home/joinmarket/menu.update.sh
+  /home/joinmarket/menu.sh
+  ;;
+REBOOT)
+  clear
+  confirmation "Are you sure?" "Reboot" "Cancel" true 7 40
+  confirmationReboot=$?
+  if [ $confirmationReboot -eq 0 ]; then
     clear
-    confirmation "Are you sure?" "Reboot" "Cancel" true 7 40
-    confirmationReboot=$?
-    if [ $confirmationReboot -eq 0 ]; then
-        clear
-        stopYG
-        echo
-        if [ "${runningEnv}" = raspiblitz ]; then
-          sudo /home/admin/XXshutdown.sh reboot
-          exit 0
-        else
-          echo "# Reboot"
-          sudo shutdown now -r
-        fi
+    stopYG
+    echo
+    if [ "${runningEnv}" = raspiblitz ]; then
+      sudo /home/admin/config.scripts/blitz.shutdown.sh reboot
+      exit 0
+    else
+      echo "# Reboot"
+      sudo shutdown now -r
     fi
-    /home/joinmarket/menu.sh;;
-  SHUTDOWN)
+  fi
+  /home/joinmarket/menu.sh
+  ;;
+SHUTDOWN)
+  clear
+  confirmation "Are you sure?" "Shutdown" "Cancel" true 7 40
+  confirmationShutdown=$?
+  if [ $confirmationShutdown -eq 0 ]; then
     clear
-    confirmation "Are you sure?" "Shutdown" "Cancel" true 7 40
-    confirmationShutdown=$?
-    if [ $confirmationShutdown -eq 0 ]; then
-      clear
-      stopYG
-      echo
-      if [ "${runningEnv}" = raspiblitz ]; then
-        sudo /home/admin/XXshutdown.sh
-        exit 0
-      else
-        echo "# Shutdown"
-        sudo shutdown now
-      fi
+    stopYG
+    echo
+    if [ "${runningEnv}" = raspiblitz ]; then
+      sudo /home/admin/config.scripts/blitz.shutdown.sh
+      exit 0
+    else
+      echo "# Shutdown"
+      sudo shutdown now
     fi
-    /home/joinmarket/menu.sh;;
-  BLITZ)
-    sudo su - admin;;
-  *)
-    clear
-    echo "
+  fi
+  /home/joinmarket/menu.sh
+  ;;
+BLITZ)
+  sudo su - admin
+  ;;
+*)
+  clear
+  echo "
 ***************************
 * JoinMarket command line *
 ***************************
@@ -152,4 +174,5 @@ https://github.com/openoms/bitcoin-tutorials/blob/master/joinmarket/README.md
 To open the JoininBox menu use: menu
 To exit from the terminal type: exit
 "
+  ;;
 esac
