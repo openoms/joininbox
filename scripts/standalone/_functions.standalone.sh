000b3775f057e678a52ff90e41466bdd95f442b5
openoms <oms@tuta.io>
2023-05-28 18:54:54 +0100

    feat: add pruned.host4coins.net, fix data owner

--- scripts/standalone/_functions.standalone.sh
+++ scripts/standalone/_functions.standalone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function addUserStore() {
-  if [ ! -d /home/store/app-data ];then
+  if [ ! -d /home/store/app-data ]; then
     echo "# Adding the user: store"
     sudo adduser --disabled-password --gecos "" store
     sudo -u store mkdir /home/store/app-data
@@ -11,7 +11,7 @@ function addUserStore() {
 }
 
 function moveSignetData() {
-  if [ -f /etc/systemd/system/signetd.service ];then
+  if [ -f /etc/systemd/system/signetd.service ]; then
     sudo systemctl stop signetd
     sudo mv /home/joinmarket/.bitcoin /home/store/app-data/
     sudo ln -s /home/store/app-data/.bitcoin /home/bitcoin/
@@ -20,7 +20,7 @@ function moveSignetData() {
   fi
 }
 
-function makeEncryptedFolder(){
+function makeEncryptedFolder() {
   # TODO
   # make encrypted file
 
@@ -33,9 +33,20 @@ function makeEncryptedFolder(){
 }
 
 function downloadSnapShot() {
+
+  if [ $#1 -eq 0 ] || [ "$1" = "pruned.host4coins.net" ]; then
+    hashFileName="sha256sum.txt.asc"
+    downloadDomain="pruned.host4coins.net"
+    pgpKeyLink="https://keys.openpgp.org/vks/v1/by-fingerprint/440C15769D19E6908CC1DDB23070DE9772DB8A48"
+  elif [ "$1" = "prunednode.today" ]; then
+    hashFileName="latest.signed.txt"
+    downloadDomain="prunednode.today"
+    pgpKeyLink="https://stepansnigirev.com/ss-specter-release.asc"
+  fi
+
   echo "# Check available diskspace"
   FREE=$(df -k --output=avail "$PWD" | tail -n1) # df -k not df -h
-  if [ $FREE -lt 12582912 ];then                 # 12G = 12*1024*1024k
+  if [ $FREE -lt 12582912 ]; then                # 12G = 12*1024*1024k
     echo "# The free space is only $FREE bytes!"
     echo "# Would need ~12GB free space to download and extract the snapshot."
     echo "# Press ENTER to continue to download regardless or CTRL+C to exit."
@@ -45,20 +56,17 @@ function downloadSnapShot() {
   fi
   sudo -u joinmarket mkdir /home/joinmarket/download 2>/dev/null
   cd /home/joinmarket/download || exit 1
-  hashFileName="latest.signed.txt"
-  sudo rm $hashFileName
-  wget https://prunednode.today/$hashFileName || exit 1
-  downloadFileName=$(grep .zip < $hashFileName | awk '{print $2}')
-  downloadLink="https://prunednode.today/$downloadFileName"
 
-  echo "# Import the PGP keys of Stepan Snigirev"
-  curl https://stepansnigirev.com/ss-specter-release.asc | gpg --import || exit 1
+  sudo rm $hashFileName
+  wget https://$downloadDomain/$hashFileName || exit 1
+  downloadFileName=$(grep .zip <$hashFileName | awk '{print $2}')
+  downloadLink="https://$downloadDomain/$downloadFileName"
+
+  echo "# Import the signing key"
+  curl -sS "$pgpKeyLink" | gpg --import || exit 1
 
   echo "# Verifying the signature of the hash ..."
-  verifyResult=$(gpg --verify $hashFileName 2>&1)
-  goodSignature=$(echo ${verifyResult} | grep -c 'Good signature')
-  echo "# goodSignature (${goodSignature})"
-  if [ ${goodSignature} -eq 0 ];then
+  if ! gpg --verify $hashFileName; then
     echo "# Invalid signature on $hashFileName"
     echo "# Press ENTER to remove the invalid file or CTRL+C to abort."
     read key
@@ -67,7 +75,7 @@ function downloadSnapShot() {
     exit 1
   fi
 
-  if [ ! -f $downloadFileName ];then
+  if [ ! -f $downloadFileName ]; then
     echo
     echo "# Downloading $downloadLink ..."
     echo
@@ -75,10 +83,7 @@ function downloadSnapShot() {
   fi
 
   echo "# Verifying the hash (takes time) ..."
-  verifyHash=$(sha256sum -c $hashFileName 2>&1)
-  goodHash=$(echo ${verifyHash} | grep -c OK)
-  echo "# goodHash ($goodHash)"
-  if [ ${goodSignature} -lt 1 ] || [ ${goodHash} -lt 1 ]; then
+  if ! sha256sum -c $hashFileName --ignore-missing; then
     echo
     echo "# Download failed --> the PGP signature did not match / signature(${goodSignature})"
     echo "# Press ENTER to remove the invalid files or CTRL+C to abort."
@@ -92,9 +97,9 @@ function downloadSnapShot() {
     echo "# The PGP signature and the hash of the downloaded snapshot is correct"
   fi
 
-  echo "# Exracting to /home/store/app-data/.bitcoin ..."
+  echo "# Extracting to /home/store/app-data/.bitcoin ..."
   FREE=$(df -k --output=avail "$PWD" | tail -n1) # df -k not df -h
-  if [ $FREE -lt 7340032 ];then                 # 7G = 7*1024*1024k
+  if [ $FREE -lt 7340032 ]; then                 # 7G = 7*1024*1024k
     echo "# The free space is only $FREE bytes!"
     echo "# Would need ~7GB free space to extract the snapshot."
     echo "# Press ENTER to continue to download regardless or CTRL+C to exit."
@@ -103,31 +108,33 @@ function downloadSnapShot() {
     echo "# OK, more than 7GB is free!"
   fi
   addUserStore
-  sudo mkdir -p /home/store/app-data/.bitcoin
+  if [ ! -d /home/store/app-data/.bitcoin ]; then
+    sudo mkdir -p /home/store/app-data/.bitcoin
+  fi 
+  echo "# Making sure user: bitcoin exists"
+  sudo adduser --disabled-password --gecos "" bitcoin
+  sudo chown -R bitcoin:bitcoin /home/store/app-data/.bitcoin
   echo "# Make sure bitcoind is not running"
   sudo systemctl stop bitcoind
-  if [ -f /home/bitcoin/.bitcoin/bitcoin.conf ];then
+  if [ -f /home/bitcoin/.bitcoin/bitcoin.conf ]; then
     echo "# Back up bitcoin.conf"
     sudo -u bitcoin mv /home/bitcoin/.bitcoin/bitcoin.conf \
-    /home/bitcoin/.bitcoin/bitcoin.conf.backup
+      /home/bitcoin/.bitcoin/bitcoin.conf.backup
   fi
   echo "# Check unzip"
   sudo apt-get install -y unzip
   sudo unzip -o $downloadFileName -d /home/store/app-data/.bitcoin
-  if [ -f /home/bitcoin/.bitcoin/bitcoin.conf.backup ];then
+  if [ -f /home/bitcoin/.bitcoin/bitcoin.conf.backup ]; then
     echo "# Restore bitcoin.conf"
     sudo -u bitcoin mv -f /home/bitcoin/.bitcoin/bitcoin.conf.backup \
-    /home/bitcoin/.bitcoin/bitcoin.conf
+      /home/bitcoin/.bitcoin/bitcoin.conf
   fi
-  echo "# Making sure user: bitcoin exists"
-  sudo adduser --disabled-password --gecos "" bitcoin
-  sudo chown -R bitcoin:bitcoin /home/store/app-data/.bitcoin
 }
 
 function installBitcoinCoreStandalone() {
   downloadBitcoinCore
 
-  if [ -f /home/bitcoin/bitcoin/bitcoind ];then
+  if [ -f /home/bitcoin/bitcoin/bitcoind ]; then
     installedVersion=$(/home/bitcoin/bitcoin/bitcoind --version | grep version)
     echo "${installedVersion} is already installed"
   else
@@ -138,20 +145,19 @@ function installBitcoinCoreStandalone() {
     cd /home/joinmarket/download/bitcoin-${bitcoinVersion}/bin/ || exit 1
     sudo install -m 0755 -o root -g root -t /home/bitcoin/bitcoin ./*
   fi
-  if [ "$(grep -c "/home/bitcoin/bitcoin" < /etc/profile)" -eq 0 ];then
+  if [ "$(grep -c "/home/bitcoin/bitcoin" </etc/profile)" -eq 0 ]; then
     echo "# Add /home/bitcoin/bitcoin to global PATH"
     echo "PATH=/home/bitcoin/bitcoin:$PATH" | sudo tee -a /etc/profile
   fi
-  installed=$(/home/bitcoin/bitcoin/bitcoind --version | grep -c "Bitcoin Core version")
-  if [ ${installed} -lt 1 ]; then
+  if ! /home/bitcoin/bitcoin/bitcoind --version | grep -c "Bitcoin Core version"; then
     echo
-    echo "!!! BUILD FAILED --> Was not able to install Bitcoin Core)"
+    echo "# BUILD FAILED --> Was not able to install Bitcoin Core)"
     exit 1
   fi
 
   # bitcoin.conf
-  if [ -f /home/store/app-data/.bitcoin/bitcoin.conf ];then
-    if [ $(grep -c rpcpassword < /home/store/app-data/.bitcoin/bitcoin.conf) -eq 0 ];then
+  if [ -f /home/store/app-data/.bitcoin/bitcoin.conf ]; then
+    if [ $(grep -c rpcpassword </home/store/app-data/.bitcoin/bitcoin.conf) -eq 0 ]; then
       sudo rm /home/store/app-data/.bitcoin/bitcoin.conf
     fi
   fi
@@ -161,14 +167,14 @@ function installBitcoinCoreStandalone() {
   sudo mkdir -p /home/store/app-data/.bitcoin
   sudo ln -s /home/store/app-data/.bitcoin /home/bitcoin/
 
-  if [ ! -f /home/bitcoin/.bitcoin/bitcoin.conf ];then
-    randomRPCpass=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c20)
+  if [ ! -f /home/bitcoin/.bitcoin/bitcoin.conf ]; then
+    randomRPCpass=$(tr </dev/urandom -dc _A-Z-a-z-0-9 | head -c20)
     echo "
 # bitcoind configuration for mainnet
 
 # Connection settings
 rpcuser=joininbox
-rpcpassword=$randomRPCpass
+rpcpassword="$randomRPCpass"
 
 prune=1000
 server=1
@@ -219,7 +225,7 @@ WantedBy=multi-user.target
   echo "# OK - the bitcoind.service is now enabled"
 
   # add aliases
-  if [ $(alias | grep -c "sudo -u bitcoin /home/bitcoin/bitcoin/bitcoin-cli") -eq 0 ];then
+  if [ $(alias | grep -c "sudo -u bitcoin /home/bitcoin/bitcoin/bitcoin-cli") -eq 0 ]; then
     sudo bash -c "echo 'alias bitcoin-cli=\"sudo -u bitcoin /home/bitcoin/bitcoin/bitcoin-cli\"' >> /home/joinmarket/_aliases.sh"
     sudo bash -c "echo 'alias bitcoind=\"sudo -u bitcoin /home/bitcoin/bitcoin/bitcoind\"' >> /home/joinmarket/_aliases.sh"
   fi
@@ -234,7 +240,7 @@ WantedBy=multi-user.target
   echo "# Monitor the bitcoind with: sudo tail -f /home/bitcoin/.bitcoin/mainnet/debug.log"
   echo
 
-  if [ ! -f /home/bitcoin/.bitcoin/mainnet/wallets/wallet.dat/wallet.dat ];then
+  if [ ! -f /home/bitcoin/.bitcoin/mainnet/wallets/wallet.dat/wallet.dat ]; then
     echo "# Create wallet.dat ..."
     sleep 10
     sudo -u bitcoin /home/bitcoin/bitcoin/bitcoin-cli -named createwallet wallet_name=wallet.dat descriptors=false
