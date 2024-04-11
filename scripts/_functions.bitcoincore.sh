e2e540ccbd643ca5c205d6eac38e9edfb3b42eb2
openoms <43343391+openoms@users.noreply.github.com>
2024-04-11 16:58:10 +0200

    bitcoin core update to 26.1 (#152)

--- scripts/_functions.bitcoincore.sh
+++ scripts/_functions.bitcoincore.sh
@@ -9,7 +9,7 @@ joininConfPath="/home/joinmarket/joinin.conf"
 function downloadBitcoinCore() {
   # set version
   # https://bitcoincore.org/en/download/
-  bitcoinVersion="26.0"
+  bitcoinVersion="26.1"
 
   if bitcoin-cli --version | grep $bitcoinVersion >/dev/null; then
     echo "# Bitcoin Core $bitcoinVersion is already installed"
